50fa855e6d04a4048e7506510dd4346b132a0732
Boris <raouphboris19@gmail.com>
2025-06-22 14:45:56 +0100

    implementation des personnage

---
+++ Personnage.py
@@ -0,0 +1,14 @@
+class Personnage:
+    def __init__(self, nom, classe, niveau, points_de_vie, force, intelligence):
+        self.nom = nom
+        self.classe = classe
+        self.niveau = niveau
+        self.points_de_vie = points_de_vie
+        self.force = force
+        self.intelligence = intelligence
+
+
+    def afficher_info(self):
+        print(f"Nom:{self.nom}")
+        print(f"classe:{self.classe}")
+        print(f"Niveau:{self.niveau}")
---
+++ main.py
@@ -0,0 +1,5 @@
+from Personnage import Personnage
+ 
+#Exemple d'utilisation de la classe Personnage
+
+joueur1 = Personnage("Aragorn", "Guerrier", 10, 100, 15, 5)
