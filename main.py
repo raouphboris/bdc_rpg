2356aa7211c8f9b0a79b0a74c13e625709e58357
Boris <raouphboris19@gmail.com>
2025-06-22 14:51:12 +0100

    Amelioration de l'affichage du Personnage

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Personnage import Personnage
  
 #Exemple d'utilisation de la classe Personnage
 
-joueur1 = Personnage("Aragorn", "Guerrier", 10, 100, 15, 5)
+joueur1 = Personnage("Aragorn", "Guerriesssss211", 10, 100, 15, 5)
